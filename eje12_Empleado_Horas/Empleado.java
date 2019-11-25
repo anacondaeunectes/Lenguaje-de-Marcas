2e5c4f9004ddc7da6fb5ba3cbe6b5150e5b6334f
anacondaeunectes <peiboligutierrez@gmail.com>
2019-11-25 13:45:08 +0100

    Añado ej 12-13-14

---
+++ eje12_Empleado_Horas/Empleado.java
@@ -0,0 +1,25 @@
+package eje12_Empleado_Horas;
+
+public class Empleado {
+	private String nombre;
+	private double horas_trabajadas;
+	private double tarifa_hora;
+	
+	public Empleado(String nombre, double horas_trabajadas, double tarifa_hora) {
+		this.nombre=nombre;
+		this.horas_trabajadas=horas_trabajadas;
+		this.tarifa_hora=tarifa_hora;
+	}
+	
+	public double getSueldoBruto() {
+		if (horas_trabajadas <= 40) {
+			return horas_trabajadas*tarifa_hora;
+		} else {
+			return 40*tarifa_hora+((horas_trabajadas-40)*(tarifa_hora*1.5));
+		}
+	}
+	
+	public String getInformacion() {
+		return nombre+" trabajo "+horas_trabajadas+" horas, cobra "+tarifa_hora+" euros la hora por lo que le corresponde un sueldo de "+getSueldoBruto()+" euros.";
+	}
+}
---
+++ eje14_Marcianos/Marte.java
@@ -0,0 +1,29 @@
+package eje14_Marcianos;
+
+public class Marte {
+
+	public static void main(String[] args) {
+		Marciano et1 = new Marciano("Wally");
+		System.out.println(et1.nace());
+		
+		
+		Marciano et2= new Marciano("Poppy");
+		System.out.println(et2.nace());
+		
+		Marciano et3= new Marciano("Mars Attack");
+		System.out.println(et3.nace());
+		
+		System.out.println(et2.muere());
+		
+		Marciano et4 = new Marciano("Bob");
+		System.out.println(et4.nace());
+		
+		System.out.println(et2.muere());
+		
+		System.out.println(et1.estaVivo());
+		System.out.println(et2.estaVivo());
+		System.out.println(et3.estaVivo());
+		System.out.println(et4.estaVivo());
+	}
+
+}
